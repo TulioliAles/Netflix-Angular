27c76338a8fe9d3d82a9c52729d83f8ac5a753f5
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-06-03 17:37:48 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'netflix';
+  
+  listTitles = [{
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 1',
+    relevance: 98,
+    age: 0,
+    parts: 2,
+    categories: ['Épico', 'Ficção', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 2',
+    relevance: 89,
+    age: 12,
+    parts: 1,
+    categories: ['Épico', 'Ficção', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia 3',
+    relevance: 95,
+    age: 14,
+    parts: 2,
+    categories: ['Épico', 'Ficção', 'Viagem no espaço']
+  }];
 }
